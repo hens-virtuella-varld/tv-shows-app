c908a20fd227a9145f4832a632f02a651eba94fb
Alessio Maffeis <alessio.maffeis@me.com>
2023-02-22 20:23:07 +0100

    split project in components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 import showsData from  './shows-data'
-
-function Show(props) {
-  return (
-    <div className='Show'>
-      <img
-        className='Show-image'
-        src={ props.showObject.image.medium} 
-        alt={ props.showObject.name } />
-      <h2>{ props.showObject.name }</h2>
-    </div>
-  )
-}
+import ShowList from './components/ShowList';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>The Best TV Shows App</h1>
-      { showsData.map(x => <Show showObject={x} />) }
+      <ShowList shows={showsData} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="Logo"></div>
+      
+      <div className="Navigation">
+        <a href="/">Home</a>
+        <a href="/favorites">Favorites</a>
+        <a href="/upcoming">Upcoming</a>
+      </div>
+
+      <div className="Search"></div>
+    </div>
+  )
+}
+
+export default Header
